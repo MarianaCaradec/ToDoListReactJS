44500fb5d156d4dbd1c3aec01fd5dcf2c6af74a3
Mariana Caradec <maricaradec@gmail.com>
2024-09-09 12:49:43 -0300

    first commit

---
+++ src/components/ToDoList/ToDoList.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+import './ToDoList.css'
+
+import { v4 as uuidv4 } from 'uuid';
+
+
+const ToDoList = () => {
+    const [tasks, setTasks] = useState([])
+    const [task, setTask] = useState('')
+
+    const addTask = () => {
+        if(task.trim()) {
+            const newTask = {
+                id: uuidv4(),
+                text: task,
+                isDone: false
+            }
+            setTasks([...tasks, newTask])
+            setTask('')
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === 'Enter'){
+            addTask()
+        }
+    }
+
+    const taskDone = (id) => {
+        const currentTasks = tasks.map(task => task.id === id ? 
+            {...task, isDone: !task.isDone} 
+            : task)
+        setTasks(currentTasks)
+    }
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter(task => task.id !== id))
+    }
+
+    return (
+        <div className= 'form'>
+            <div>
+                <label>Tarea</label>
+                <input type="text" value={task} 
+                onChange={(e) => setTask(e.target.value)} 
+                onKeyDown={handleKeyDown}/>
+                <button className='buttonAdd' onClick={()=>addTask()} >Agregar tarea</button>
+            </div>
+            <div>
+                <ul className='list'>
+                    {tasks.length > 0 ? tasks.map((task) => (
+                            <li key={task.id}>
+                                <button className={task.isDone ? 'taskDone' : 'task'}  
+                                onClick={() => taskDone(task.id)}> {task.text} </button> 
+                                <button onClick={() => deleteTask(task.id)}>Eliminar tarea</button>
+                            </li>
+                    )) : <p>No hay tareas añadidas en este momento</p>}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ToDoList
